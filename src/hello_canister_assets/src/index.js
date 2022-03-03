b29b92ba502b45d580a9e9c1fb961f2c3bd73544
nghiavoint <nghiavo051095@gmail.com>
2022-03-03 00:05:55 -0500

    first commit

---
+++ src/hello_canister_assets/src/index.js
@@ -0,0 +1,19 @@
+import { hello_canister } from "../../declarations/hello_canister";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await hello_canister.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
